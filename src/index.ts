fd9acd83bd9c1eafd479ab74540db1085d17cf8a
MarcLipe <marcos.lima52@fatec.sp.gov.br>
2023-06-18 16:37:43 -0300

    Install EJS with Typescript

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,20 @@
 import express from "express";
+import path from "path";
+
 const app = express();
 
-app.get("/", function (req, res) {
-  res.send("Hello World");
+// Configuração do EJS
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Porta que o servidor vai escutar
+const port = 3000;
+
+//Rota
+app.get("/", (req, res) => {
+  res.render("index", { message: "Hello, EJS!" });
 });
 
-app.listen(3000);
+app.listen(port, () => 
+  console.log(`Servidor rodando em http://localhost:${port}`)
+);
